f19c61bee2287cc1af1df3d97bbc856461d8fe01
ChamodDamitha <chamod.14@cse.mrt.ac.lk>
2016-12-08 21:28:38 +0530

    GPS detection works

--- app/src/main/java/com/example/shan/location/MainActivity.java
+++ app/src/main/java/com/example/shan/location/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.shan.location;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2;
-    LocationManager locationManager;
-    MyLocationListener locationListener;
     TextView textView;
-
+    MyLocationListener locationListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +19,45 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.txtView);
 
-        locationListener=new MyLocationListener(this);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0,
+                new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        String currentLocation = ""
+                                + location.getLatitude()
+                                + " ,"
+                                + location.getLongitude();
+
+                        Toast.makeText(MainActivity.this, currentLocation, Toast.LENGTH_SHORT).show();
+                        textView.setText(currentLocation);
 
 
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                });
+
+            //locationListener=new MyLocationListener(this);
     }
 
     public void clickk(View view) {
 
-        textView.setText("lat:"+locationListener.getLatitude()+"\n"+"lon:"+locationListener.getLongitude());
+        //textView.setText("lat:"+locationListener.getLatitude()+"\n"+"lon:"+locationListener.getLongitude());
+        System.exit(0);
     }
 
 
